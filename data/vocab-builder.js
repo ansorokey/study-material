09105c6a04e3ff006816b8c0d6235fb4fe9cdf08
Anton Sorokey <ansorokey@gmail.com>
2025-07-18 14:46:55 -0400

    basic button selection setup

---
+++ data/vocab-builder.js
@@ -0,0 +1,140 @@
+const groupA = [
+    {
+        kanji: '口',
+        kana: 'くち',
+        meaning: 'Mouth',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '目',
+        kana: 'め',
+        meaning: 'Eye',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '耳',
+        kana: 'みみ',
+        meaning: 'Ear',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '鼻',
+        kana: 'はな',
+        meaning: 'Nose',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '顔',
+        kana: 'かお',
+        meaning: 'Face',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '手',
+        kana: 'て',
+        meaning: 'Hand',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '足',
+        kana: 'あし',
+        meaning: 'Leg, Foot',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '指',
+        kana: 'ゆび',
+        meaning: 'Finger',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '頭',
+        kana: 'あたま',
+        meaning: 'Head',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '歯',
+        kana: 'は',
+        meaning: 'Tooth, Teeth',
+        visual: '',
+        visualType: ''
+    },
+]
+
+const groupB = [
+    {
+        kanji: '枕',
+        kana: 'まくら',
+        meaning: 'Pillow',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: 'ベッド',
+        kana: 'ベッド',
+        meaning: 'Bed',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '布団',
+        kana: 'ふとん',
+        meaning: 'Futon',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '毛布',
+        kana: 'もうふ',
+        meaning: 'Blanket',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: 'タオル',
+        kana: 'タオル',
+        meaning: 'Towel',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: 'お風呂',
+        kana: 'おふろ',
+        meaning: 'Bath',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '石鹸',
+        kana: 'せっけん',
+        meaning: 'Soap',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '歯ブラシ',
+        kana: 'はぶらし',
+        meaning: 'Toothbrush',
+        visual: '',
+        visualType: ''
+    },{
+        kanji: '鏡',
+        kana: 'かがみ',
+        meaning: 'Mirror',
+        visual: '',
+        visualType: ''
+    },
+    {
+        kanji: '窓',
+        kana: 'まど',
+        meaning: 'Window',
+        visual: '',
+        visualType: ''
+    },
+]
+
+const list = [
+    {
+        'group': groupA,
+        'name': 'The Body'
+    },{
+        'group': groupB,
+        'name': 'Bed and Bath'
+    },
+]
+
+export default list;
